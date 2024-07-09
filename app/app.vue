9dc5def61ceda535272b1f8b30a9f6939db51d6c
ririo08 <ririo08@users.noreply.github.com>
2024-07-10 01:10:12 +0900

    feat: メインロジック

--- app/app.vue
+++ app/app.vue
@@ -1,30 +1,136 @@
 <script setup lang="ts">
+// 背景色
 useHead({
   bodyAttrs: {
-    class: 'dark:bg-gray-900'
-  }
+    class: 'dark:bg-gray-900',
+  },
 })
 
+// バージョン
 const version = ref('')
 onMounted(async () => {
   try {
     const response = await fetch('version')
     const data = await response.text()
     version.value = data
-  } catch (error) {
+  }
+  catch (error) {
     console.error('failed version fetch:', error)
   }
+})
+
+// ロジック
+interface User {
+  id: number
+  name: string
+  time?: number
+}
+const users: Ref<User[]> = ref([])
+const addUser = () => {
+  users.value.push({
+    id: users.value.length + 1,
+    name: '',
+    time: undefined,
+  })
+}
+
+const result = computed(() => {
+  const sortedUsers = users.value
+    .filter(w => typeof w.time !== 'undefined')
+    .sort((a, b) => b.time! - a.time!)
+  const time: string[] = []
+  let i = 1
+  for (const user of sortedUsers) {
+    if (i === 1) {
+      time.push(`${i}. ${user.name}`)
+    }
+    else {
+      const firstUser = sortedUsers[0]
+      time.push(`${i}. ${user.name} → ${firstUser?.name}の${firstUser!.time! - user.time!}秒後`)
+    }
+    i++
+  }
+  return time.join('\n')
+})
+
+const { copy, copied, isSupported } = useClipboard()
+
+const demo = () => {
+  users.value = [{
+    id: 1,
+    name: 'Aさん',
+    time: 50,
+  }, {
+    id: 2,
+    name: 'Bさん',
+    time: 20,
+  }]
+}
+
+onMounted(() => {
+  addUser()
+  addUser()
 })
 </script>
 
 <template>
-  <div class="max-w-xl m-auto mt-4">
+  <div class="max-w-xl m-auto mt-4 px-2 sm:px-0 mx-auto">
     <h1 class="text-2xl font-bold">
-      ホワイトアウトサバイバル 波状計算機 ver {{ version }}
+      ホワイトアウトサバイバル 波状計算機
     </h1>
+    <h2 class="text-xl text-right font-bold">
+      ver {{ version }}
+    </h2>
     <p class="mt-1">
-      srtファイルからテキスト情報のみを抽出してダウンロードすることができます。<br>
-      事前に結果も確認することができます。
+      波状攻撃の計算を行います。
+      <ULink
+        class="text-sky-500 hover:text-sky-800"
+        @click="demo"
+      >
+        サンプル
+      </ULink>
     </p>
+    <section class="mt-4">
+      <div
+        v-for="user of users"
+        :key="user.id"
+        class="columns-2 mt-2"
+      >
+        <UFormGroup label="名前">
+          <UInput v-model="user.name" />
+        </UFormGroup>
+        <UFormGroup label="時間">
+          <UInput
+            v-model="user.time"
+            type="number"
+          />
+        </UFormGroup>
+      </div>
+    </section>
+    <div class="text-center mt-4">
+      <UButton
+        icon="i-heroicons-plus"
+        @click="addUser"
+      >
+        追加
+      </UButton>
+    </div>
+    <section v-if="result">
+      <div class="text-right">
+        <UButton
+          :icon="copied ? 'i-heroicons-check' : 'i-heroicons-clipboard'"
+          color="blue"
+          :disabled="!isSupported"
+          @click="copy(result)"
+        >
+          コピー
+        </UButton>
+      </div>
+      <UCard
+        class="mt-4"
+      >
+        <pre>{{ result }}</pre>
+      </UCard>
+    </section>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@vueuse/nuxt'],
+
   app: {
-    baseURL: '/ws-undulating-calculator/'
+    baseURL: '/ws-undulating-calculator/',
+    head: {
+      title: 'ホワイトアウトサバイバル 波状計算機',
+    },
   },
+
   future: {
     compatibilityVersion: 4,
   },
 
   eslint: {
     config: {
-      stylistic: true
-    }
-  }
+      stylistic: true,
+    },
+  },
 })
